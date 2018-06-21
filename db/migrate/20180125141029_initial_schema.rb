e86932d7a8500a397a981d3f51d2f1ce24fee541
waterlow <waterlow3442@gmail.com>
2018-06-22 01:15:49 +0900

    force cascadeはmigrationファイルには不要

--- db/migrate/20180125141029_initial_schema.rb
+++ db/migrate/20180125141029_initial_schema.rb
@@ -1,11 +1,11 @@
 class InitialSchema < ActiveRecord::Migration[5.1]
   def change
-    create_table :contributors, force: :cascade do |t|
+    create_table :contributors do |t|
       t.string :name, null: false, index: { unique: true }
       t.timestamps
     end
 
-    create_table :episodes, force: :cascade do |t|
+    create_table :episodes do |t|
       t.string :identifier, null: false, index: { unique: true }
       t.string :title, null: false
       t.string :description, null: false
@@ -14,13 +14,13 @@ class InitialSchema < ActiveRecord::Migration[5.1]
       t.timestamps
     end
 
-    create_table :episode_contributors, force: :cascade do |t|
+    create_table :episode_contributors do |t|
       t.references :episode, null: false, foreign_key: true
       t.references :contributor, null: false, foreign_key: true
       t.timestamps
     end
 
-    create_table :show_notes, force: :cascade do |t|
+    create_table :show_notes do |t|
       t.references :episode, null: false, foreign_key: true
       t.string :text, null: false
       t.text :url, null: false
